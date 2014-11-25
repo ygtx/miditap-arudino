ad02daf3c6c493baa82b137436e0144b4623ca54
八木達也 <mac@maci.local>
2014-11-26 08:39:03 +0900

    anyway commit

--- sketch_may11b_prod.ino
+++ sketch_may11b_prod.ino
@@ -1,5 +1,5 @@
 int bit2btn[16] = {
-   7, 11, 10,  6,  9,  5,  1,  2, // D3
+  7, 11, 10,  6,  9,  5,  1,  2, // D3
   -1, -1, -1, -1,  8, 12,  3,  4  // D4
 };
 
@@ -33,54 +33,61 @@ void data2button() {
   digitalWrite(clock,LOW);
 
   digitalWrite(latch,HIGH);
-//  delayMicroseconds(1000);
-  digitalWrite(latch,LOW);
+  delayMicroseconds(2);
 
   if (digitalRead(data3) == 0) {
     buttons[bit2btn[7]] = true;
-  } else {
+  } 
+  else {
     buttons[bit2btn[7]] = false;
   }
   if (digitalRead(data4) == 0) {
     buttons[bit2btn[15]] = true;
-  } else {
+  } 
+  else {
     buttons[bit2btn[15]] = false;
   }
 
+  digitalWrite(latch,LOW);
+
   for (int i = 1; i <= 7; i++) {
     digitalWrite(clock,HIGH);
-//    delayMicroseconds(2000);
+    delayMicroseconds(2);
 
     if (digitalRead(data3) == 0) {
       buttons[bit2btn[7-i]] = true;
-    } else {
+    } 
+    else {
       buttons[bit2btn[7-i]] = false;
     }
     if (digitalRead(data4) == 0) {
       if (bit2btn[15-i] != -1) {
         buttons[bit2btn[15-i]] = true;
       }
-    } else {
+    } 
+    else {
       buttons[bit2btn[15-i]] = false;
     }
 
-//    delayMicroseconds(4000);
+    delayMicroseconds(4);
     digitalWrite(clock,LOW);
   }
 }
 
 void printButtons() {
+  String signal = String(27);
+  signal = String("s,");
   for (int row = 0; row < 3; row++) {
     for (int col = 0; col < 4; col++) {
-      Serial.print(String(buttons[row*4 + col + 1]) + ",");
+      signal += String(buttons[row*4 + col + 1]) + ",";
     }
   }
-  Serial.print("/");
-  Serial.println();
+  signal += String("e,");
+  Serial.print(signal);
 }
 
 void loop() {
   data2button();
   printButtons();
-//  delay(1000);
+  delay(10);
 }
